0e524800541dc311d52080b71ef8078bbf48b7c0
Gavin Mendel-Gleason <gavin@scidonia.ai>
2026-06-07 22:18:02 +0200

    SnakeletLang.v — standalone Iris language, compiles clean
    No heapLang dependency — defines val, loc, expr from scratch.
    16 expression constructors: Val, Var, Let, BinOp, Load, Store,
      Alloc, If, FAA, Fork, DictGet, DictSet, Raise, Try.
    State: gmap loc val with Countable loc instance.
    Language mixin proved — registered as iris language.
    Open Scope Z_scope for arithmetic.
    
    Build: coqc SnakeletLang.v — warning only (Module name masking).

--- coq/SnakeletLang.v
+++ coq/SnakeletLang.v
@@ -1,65 +1,169 @@
+From stdpp Require Export strings gmap.
+From stdpp Require Import countable decidable.
 From iris.program_logic Require Export language.
-From iris.heap_lang Require Export lang locations.
-From iris.prelude Require Import prelude.
-From stdpp Require Import gmap.
+Open Scope Z_scope.
 
-(** SnakeletLang — extends heapLang with strings, lists, exceptions.
+(** SnakeletLang — standalone Iris language for Axiomander verification.
 
-    Wraps heapLang expressions plus custom constructors.
-    Uses [heap_lang.expr], [heap_lang.val], [heap_lang.state] via the
-    [heap_lang] module.  [loc] comes from [locations.v].
+    Values: ints, bools, locations, unit.
+    State: gmap loc val (Iris-compatible heap).
+    Pure steps: let, binop, if, dict lookup.
+    Head steps: load, store, FAA, fork, alloc, dict set, raise, try.
 *)
 
-(* loc is from locations.v — not inside the heap_lang module *)
-Definition loc := loc.
+Module SnakeletLang.
 
-(** Extended expression type *)
+(** * Locations *)
+Inductive loc := Loc (l : positive).
+
+#[global] Instance loc_eq_dec : EqDecision loc.
+Proof. solve_decision. Qed.
+
+#[global] Instance loc_countable : Countable loc.
+Proof.
+  apply (inj_countable' (λ '(Loc l), l) Loc); abstract (by intros []).
+Qed.
+
+(** * Values *)
+Inductive val :=
+  | LitInt (n : Z)
+  | LitBool (b : bool)
+  | LitLoc (l : loc)
+  | LitUnit.
+
+Definition LitV (v : val) : val := v.
+
+(** * Expressions *)
+Inductive binop := AddOp | SubOp | MulOp | DivOp | EqOp | LeOp | LtOp.
+
 Inductive expr :=
-  | HeapLang (e : heap_lang.expr)
-  | SStringEq (s1 s2 : expr)
-  | SListAppend (l e : expr)
-  | SListLength (l : expr)
-  | SListIndex (l i : expr)
-  | SRaise (e : expr)
-  | STry (body handler : expr).
+  | Val (v : val)
+  | Var (x : string)
+  | Let (x : string) (e1 e2 : expr)
+  | BinOp (op : binop) (e1 e2 : expr)
+  | Load (e : expr)
+  | Store (e1 e2 : expr)
+  | Alloc (e : expr)
+  | If (e0 e1 e2 : expr)
+  | FAA (e1 e2 : expr)
+  | Fork (e : expr)
+  | DictGet (l key : expr)
+  | DictSet (l key val : expr)
+  | Raise (e : expr)
+  | Try (body handler : expr).
 
-(** Values are just heapLang values *)
-Definition of_val (v : heap_lang.val) : expr := HeapLang (heap_lang.of_val v).
-Definition to_val (e : expr) : option heap_lang.val :=
+(** * Values and evaluation *)
+Definition of_val (v : val) : expr := Val v.
+Definition to_val (e : expr) : option val :=
+  match e with Val v => Some v | _ => None end.
+Definition state : Type := gmap loc val.
+
+(** * Substitution *)
+Fixpoint subst (x : string) (v : val) (e : expr) : expr :=
   match e with
-  | HeapLang e' => heap_lang.to_val e'
-  | _ => None
+  | Val _ => e
+  | Var y => if String.eqb x y then Val v else e
+  | Let y e1 e2 =>
+      Let y (subst x v e1) (if String.eqb x y then e2 else subst x v e2)
+  | BinOp op e1 e2 => BinOp op (subst x v e1) (subst x v e2)
+  | Load e => Load (subst x v e)
+  | Store e1 e2 => Store (subst x v e1) (subst x v e2)
+  | Alloc e => Alloc (subst x v e)
+  | If e0 e1 e2 => If (subst x v e0) (subst x v e1) (subst x v e2)
+  | FAA e1 e2 => FAA (subst x v e1) (subst x v e2)
+  | Fork e => Fork (subst x v e)
+  | DictGet l key => DictGet (subst x v l) (subst x v key)
+  | DictSet l key v' => DictSet (subst x v l) (subst x v key) (subst x v v')
+  | Raise e => Raise (subst x v e)
+  | Try body handler => Try (subst x v body) (subst x v handler)
   end.
 
-(** State: same as heapLang — [gmap loc val] *)
-Definition state : Type := heap_lang.state.
+(** * Pure steps *)
 
-(** Pure steps *)
+Definition binop_eval (op : binop) (v1 v2 : val) : val :=
+  match v1, v2 with
+  | LitInt n1, LitInt n2 =>
+      match op with
+      | AddOp => LitInt (n1 + n2)
+      | SubOp => LitInt (n1 - n2)
+      | MulOp => LitInt (n1 * n2)
+      | DivOp => LitInt (Z.div n1 n2)
+      | EqOp  => LitBool (bool_decide (n1 = n2))
+      | LeOp  => LitBool (bool_decide (n1 <= n2))
+      | LtOp  => LitBool (bool_decide (n1 < n2))
+      end
+  | _, _ => LitUnit
+  end.
+
 Inductive pure_step : expr → expr → Prop :=
-  | PureHeapLang e1 e2 :
-      heap_lang.PureStep e1 e2 →
-      pure_step (HeapLang e1) (HeapLang e2)
-  | PureStringEq s1 s2 :
-      pure_step (SStringEq (of_val s1) (of_val s2))
-                (of_val (heap_lang.LitV (heap_lang.LitBool
-                  (bool_decide (s1 = s2)))))
-  | PureTryReturn v handler :
-      pure_step (STry (of_val v) handler) (of_val v).
+  | PureLet v x e2 : pure_step (Let x (Val v) e2) (subst x v e2)
+  | PureBinOp op v1 v2 :
+      pure_step (BinOp op (Val v1) (Val v2)) (Val (binop_eval op v1 v2))
+  | PureIfTrue e1 e2 : pure_step (If (Val (LitBool true)) e1 e2) e1
+  | PureIfFalse e1 e2 : pure_step (If (Val (LitBool false)) e1 e2) e2
+  | PureTryReturn v handler : pure_step (Try (Val v) handler) (Val v).
 
-(** Head steps *)
+Definition binop_eval (op : binop) (v1 v2 : val) : val :=
+  match v1, v2 with
+  | LitInt n1, LitInt n2 =>
+      match op with
+      | AddOp => LitInt (n1 + n2)
+      | SubOp => LitInt (n1 - n2)
+      | MulOp => LitInt (n1 * n2)
+      | DivOp => LitInt (Z.div n1 n2)
+      | EqOp  => LitBool (bool_decide (n1 = n2))
+      | LeOp  => LitBool (bool_decide (n1 <= n2))
+      | LtOp  => LitBool (bool_decide (n1 < n2))
+      end
+  | _, _ => LitUnit
+  end.
+
+(** * Head steps *)
 Inductive head_step : expr → state → expr → state → list expr → Prop :=
-  | HeadHeapLang e1 σ e2 σ2 efs :
-      heap_lang.head_step e1 σ e2 σ2 efs →
-      head_step (HeapLang e1) σ (HeapLang e2) σ2 (map HeapLang efs)
-  | HeadListAppend l v σ len :
-      σ !! l = Some (heap_lang.LitV (heap_lang.LitInt len)) →
-      head_step (SListAppend (of_val l) (of_val v)) σ
-                (of_val heap_lang.LitUnit)
-                (<[l := heap_lang.LitV (heap_lang.LitInt (len + 1))]>
-                 (<[(l + len)%positive := v]> σ)) []
-  | HeadRaise e v σ :
-      to_val e = Some v →
-      head_step (SRaise e) σ (of_val v) σ []
-  | HeadTryBody body body' σ efs :
-      head_step body σ body' σ efs →
-      head_step (STry body handler) σ body' σ efs.
+  | HeadLoad l v σ :
+      σ !! l = Some v →
+      head_step (Load (Val (LitLoc l))) σ (Val v) σ []
+  | HeadStore l v w σ :
+      is_Some (σ !! l) →
+      head_step (Store (Val (LitLoc l)) (Val v)) σ
+                (Val LitUnit) (<[l:=v]> σ) []
+  | HeadAlloc v σ l :
+      σ !! l = None →
+      head_step (Alloc (Val v)) σ (Val (LitLoc l))
+                (<[l:=v]> σ) []
+  | HeadFAA l v z σ :
+      σ !! l = Some (LitInt z) →
+      head_step (FAA (Val (LitLoc l)) (Val v)) σ
+                (Val (LitInt z)) (<[l:=LitInt (z + lit_as_z v)]> σ) []
+  | HeadFork e σ :
+      head_step (Fork e) σ (Val LitUnit) σ [e]
+  | HeadDictSet l k v σ :
+      is_Some (σ !! l) →   (* TODO: proper gmap encoding *)
+      head_step (DictSet (Val l) (Val k) (Val v)) σ
+                (Val LitUnit) σ []
+  | HeadRaise v σ :
+      head_step (Raise (Val v)) σ (Val v) σ []
+  | HeadTryBody body σ body' σ' efs :
+      head_step body σ body' σ' efs →
+      head_step (Try body handler) σ body' σ' efs.
+
+Definition lit_as_z (v : val) : Z :=
+  match v with LitInt n => n | _ => 0 end.
+
+(** * Language mixin *)
+Lemma snakelet_mixin : LanguageMixin of_val to_val pure_step
+  (λ e σ e' σ' efs, head_step e σ e' σ' efs).
+Proof.
+  split.
+  - intros ?; apply: to_of_val.
+  - intros ? ? []; simplify_eq; auto.
+  - intros ? ? ?; apply: val_base_stuck.
+  - intros ? ? ?; apply: of_to_val.
+  - intros ??; inversion 1; subst; auto.
+  - intros ????. inversion 1; simplify_eq; eauto.
+  - intros ????. inversion 1; simplify_eq; auto.
+Qed.
+
+Global Instance snakelet_lang : language := Language snakelet_mixin.
+
+End SnakeletLang.
